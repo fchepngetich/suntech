59a88c4e27cd4a04139c81fe10d499832522a306
Faith Chepngetich <faith.chepngetich@zetech.ac.ke>
2024-09-24 18:13:29 +0300

    add all the changes

--- app/Views/backend/pages/home/home.php
+++ app/Views/backend/pages/home/home.php
@@ -66,19 +66,300 @@
     </section>
 
     <section class="introduction">
-        <div class="container mt-3 mb-3">
+        <div class="container mt-3 mb-1">
             <div class="row">
-                    <div class="content p-3">
-                        <h4 class="title text-center mb-2">Solar Power Equipment Supplier In Kenya</h4>
-                        <p class="des">
-                        Suntech Power Limited is a supplier of solar power and lighting solutions in Kenya. Registered by Energy and Petroleum Regulatory Authority (EPRA), Suntech Power supplies and installs high quality solar and energy-related equipment in Kenya and the East African region. We supply, install and maintain solar water heating systems, solar inverters, solar batteries, solar power systems, solar pumping solutions, backup systems, and solar installation accessories. Suntech has a team of qualified solar experts including engineers, solar technicians and installers who are licensed by EPRA.</p>
-                       
-                    </div>
-                
-            </div>
+                <div class="content p-3">
+                    <h4 class="title mb-2">Solar Power Equipment Supplier In Kenya</h4>
+                    <p class="des">
+                        Suntech Power Limited is a supplier of solar power and lighting solutions in Kenya. Registered
+                        by Energy and Petroleum Regulatory Authority (EPRA), Suntech Power supplies and installs high
+                        quality solar and energy-related equipment in Kenya and the East African region. We supply,
+                        install and maintain solar water heating systems, solar inverters, solar batteries, solar power
+                        systems, solar pumping solutions, backup systems, and solar installation accessories. Suntech
+                        has a team of qualified solar experts including engineers, solar technicians and installers who
+                        are licensed by EPRA.</p>
+
+                </div>
+
             </div>
         </div>
-    </section>
+</div>
+</section>
+<!-- Start Recommended -->
 
 
-    <?= $this->endSection() ?>
+<section>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+        <h4 class="title">Reccommended For You</h4>
+<span class="ml-auto"> <a class="btn-sm btn-info" href="">View All</a> </span>
+        </div>
+        <div class="row">
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured4.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured4.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+                            <div class="product-action-2">
+                                <a title="Add to cart" href="#">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured1.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured1.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+                            <div class="product-action-2">
+                                <a title="Add to cart" href="#">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured2.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured2.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+                            <div class="product-action-2">
+                                <a title="Add to cart" href="#">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured3.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured3.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+                            <div class="product-action-2">
+                                <a title="Add to cart" href="#">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured2.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured2.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+                            <div class="product-action-2">
+                                <a title="Add to cart" href="#">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-12">
+                <div class="single-product">
+                    <div class="product-img">
+                        <a href="product-details.html">
+                            <img class="default-img" src="backend/images/Featured4.png" alt="#">
+                            <img class="hover-img" src="backend/images/Featured4.png" alt="#">
+                        </a>
+                        <div class="button-head">
+                            <div class="product-action">
+                                <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+                                        class=" ti-eye"></i><span>Quick Shop</span></a>
+                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="product-content">
+                        <h3><a href="product-details.html">Women Hot Collection</a></h3>
+                        <div class="product-price">
+                            <span class="discounted-price">$19.00</span>
+                            <span class="original-price">$29.00</span>
+                        </div>
+                    </div>
+                    <div class="product-action-2 m-2">
+                                <a title="Add to cart" class="btn-sm btn-danger" href="#">Add to cart</a>
+                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Reccommended -->
+
+
+<!-- Start Sustainability  -->
+
+<div class="section-inner mb-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-12">
+                <div class="image">
+
+                    <img src="backend/images/sustainability-banner.png" alt="#">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Sustainability  -->
+
+<!-- Top Deals  -->
+
+<!-- Top Deals  -->
+
+
+
+<!-- Start Shop Blog  -->
+<section class="shop-blog sections">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-titles mb-3 mt-3">
+                    <h4>Latest Insights</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-12">
+                <!-- Start Single Blog  -->
+                <div class="shop-single-blog">
+                    <img src="https://via.placeholder.com/370x300" alt="#">
+                    <div class="content">
+                        <p class="date">22 July , 2020. Monday</p>
+                        <a href="#" class="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,
+                            magna eros eu erat. Aliquam er</a>
+                        <a href="#" class="more-btn">Continue Reading</a>
+                    </div>
+                </div>
+                <!-- End Single Blog  -->
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+                <!-- Start Single Blog  -->
+                <div class="shop-single-blog">
+                    <img src="https://via.placeholder.com/370x300" alt="#">
+                    <div class="content">
+                        <p class="date">22 July, 2020. Monday</p>
+                        <a href="#" class="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,
+                            magna eros eu erat. Aliquam er</a>
+                        <a href="#" class="more-btn">Continue Reading</a>
+                    </div>
+                </div>
+                <!-- End Single Blog  -->
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+                <!-- Start Single Blog  -->
+                <div class="shop-single-blog">
+                    <img src="https://via.placeholder.com/370x300" alt="#">
+                    <div class="content">
+                        <p class="date">22 July, 2020. Monday</p>
+                        <a href="#" class="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,
+                            magna eros eu erat. Aliquam er</a>
+                        <a href="#" class="more-btn">Continue Reading</a>
+                    </div>
+                </div>
+                <!-- End Single Blog  -->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Shop Blog  -->
+
+<?= $this->endSection() ?>
